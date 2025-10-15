f8bbbcbf7ca7c2e1ad5b9bd8059275f882a22ed3
Konishi <2511141003x@ed.fuk.kindai.ac.jp>
2025-10-15 13:43:56 +0000

    push to reder.com

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,5 +3,3 @@
 //= link_tree images
 //= link_directory stylesheets .css
 //= link_directory javascripts .js
-// もし app/javascript/controllers にもアセットがある場合：
-// = link_tree ../../javascript/controllers
