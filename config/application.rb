321eb061db49e9e85ea8db63fe6750593edf47fd
Konishi <2511141003x@ed.fuk.kindai.ac.jp>
2025-10-15 02:46:58 +0000

    push to reder.com

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-require "sprockets/railtie"
+require "active_storage/engine" 
+#require "sprockets/railtie"
 # require "rails/test_unit/railtie" # テストを必要としない場合
 
 # Require the gems listed in Gemfile, including any gems
