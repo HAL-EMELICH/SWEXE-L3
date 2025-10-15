764635a96f05a1e8d1fac27d735f9a343cc5cc04
Konishi <2511141003x@ed.fuk.kindai.ac.jp>
2025-10-15 13:18:02 +0000

    push to reder.com

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 // app/assets/config/manifest.js
 
-//= link_tree ./images
-//= link_directory ./stylesheets .css
-//= link_directory ./javascripts .js
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_directory ../javascripts .js
+//= link_directory ../../javascript .js
